ff8dfca0afabe37c1def0063a68ce821eff8c32f
Ayse <aysesokmenn@gmail.com>
2021-05-13 17:21:00 -0400

    web ex added

--- jquery-dom/exercise_2/app.js
+++ jquery-dom/exercise_2/app.js
@@ -1,4 +1,33 @@
-$(function() {
-    
+let currentId = 0;
+let moviesList = [];
 
-})
+$(function() {
+    $("#new-movie-form").on("submit", function(evt) {
+        evt.preventDefault();
+        let title = $("#title").val();
+        let rating = $("#rating").val();
+    
+        let movieData = { title, rating, currentId };
+        const HTMLtoAppend = createMovieDataHTML(movieData);
+    
+        currentId++
+        moviesList.push(movieData);
+    
+        $("#movie-table-body").append(HTMLtoAppend);
+        $("#new-movie-form").trigger("reset");
+    });
+
+    $("tbody").on("click", ".btn.btn-danger", function(evt) {
+        // find the index where this movie is
+        let indexToRemoveAt = moviesList.findIndex(movie => movie.currentId === +$(evt.target).data("deleteId"))
+        
+        // remove it from the array of movies
+        moviesList.splice(indexToRemoveAt, 1)
+    
+        // remove it from the DOM
+        $(evt.target)
+          .closest("tr")
+          .remove();
+        
+      });
+}
